b54886ff99fffa3ae5c178786202a1bd6c643c60
satvikhulikere <hulikere.satvik@gmail.com>
2025-08-08 12:44:41 -0400

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,7 +8,7 @@ import os
 
 # Constants and config
 DATA_PATH = "../workout-forecasting-ml/data/workout_data.csv"
-MODEL_DIR = "/Users/satvikhulikere/workout-forecasting-ml/models"
+MODEL_DIR = "../workout-forecasting-ml/models"
 FEATURE_COLS = ['Reps', 'Sets', 'SleepHours', 'ProteinIntake', 'Calories', 'SorenessLevel', 'Bodyweight']
 TARGET_COL = 'WeightLifted'
 SEQUENCE_LENGTH = 5
